9fa38566a421fdf4ec766da7912ed46e3676d590
Stew Esho <StewardEsho@gmail.com>
2016-03-29 10:43:53 -0400

    Added better functionality to the joystick; set FPS to 60
    The joystick now snaps to the finger instantly on the first touch.
    Touching anywhere in the background area will cause the stick to snap to the finger.
    Stick will no longer travel outside the limited area. However, upon reaching maximum magnitude, stick will unable to switch angle without first lowering the magnitude.

--- joystick.lua
+++ joystick.lua
@@ -36,6 +36,7 @@ local angle;
 local magnitude;
 local background;
 local stick;
+local deltaRadius;
 local x, y;
 local angleText;
 local magText;
@@ -57,6 +58,7 @@ function joystick.new(_x, _y)
   background:setFillColor(0.7, 0.7, 0.7)
   stick = display.newCircle(_x, _y, display.contentWidth/20);
   stick:setFillColor(0.4, 1, 0.6, 0.3);
+  deltaRadius = (3 * display.contentWidth)/40;
 
   angleText = display.newText(angle, 500, 300, "Arial", 72);
   magText = display.newText("0", 500, 500, "Arial", 72);
@@ -69,14 +71,19 @@ end
 local function onStickHold(event)
   if (event.phase == "began") then
     display.getCurrentStage():setFocus( self, event.id )
-    isStickFocus = true
-  elseif (isStickFocus == true) then
-    if (event.phase == "moved") then
-      stick.x = event.x;
-      stick.y = event.y;
-    elseif (event.phase == "ended" or event.phase == "cancelled") then
+    isStickFocus = true;
+  end
+  if (isStickFocus == true) then
+      if (joystick:getMagnitude() < 1) then
+        stick.x = event.x;
+        stick.y = event.y;
+      else
+        stick.x = background.x + (deltaRadius/math.sin(math.rad(joystick:getAngle())));
+        stick.y = background.y + (deltaRadius/-math.cos(math.rad(joystick:getAngle())));
+      end
+    if (event.phase == "ended" or event.phase == "cancelled") then
       display.getCurrentStage():setFocus( self, nil );
-      isStickFocus = false
+      isStickFocus = false;
       stick.x = background.x;
       stick.y = background.y;
     end
@@ -87,17 +94,30 @@ end
 
 --[[
 
+  getAngle
+    -returns the angle of the joystick
+    - Measured in degrees. 0 is up
+
+  getMagnitude
+    -returns the magnitude of the joystick
+    -ranges from 0 - 1
+    -calculated using center of the joystick
+
+  init
+    - runs once to initiate the joystick
+    - adds the event listener that allows the joystick to move around
+
   run
     - Runs in the game loop
     - gets user input, and returns angle and magnitude values
+
 ]]--
 
---Getters
 function joystick:getAngle()
   if (stick.x - background.x < 0) then
-    angle = math.deg(math.atan((stick.y - background.y)/(stick.x - background.x)))+270;
+    angle = math.deg(math.atan((stick.y - background.y)/(stick.x - background.x))) + 270;
   elseif (stick.x - background.x > 0) then
-    angle = math.deg(math.atan((stick.y - background.y)/(stick.x - background.x)))+90;
+    angle = math.deg(math.atan((stick.y - background.y)/(stick.x - background.x))) + 90;
   elseif (stick.x - background.x == 0) then
     angle = 0;
   end
@@ -106,11 +126,15 @@ end
 
 function joystick:getMagnitude()
   magnitude = math.sqrt(math.pow((stick.x-background.x),2) + math.pow((stick.y-background.y),2)) / (display.contentWidth/8)
-  return magnitude;
+  if (magnitude > 1) then
+    return 1;
+  else
+    return magnitude;
+  end
 end
 
 function joystick:init()
-  stick:addEventListener("touch", onStickHold);
+  background:addEventListener("touch", onStickHold);
 end
 
 function joystick:run()
