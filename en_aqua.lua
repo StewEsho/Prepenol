247074615f4c7719f90553099fc3816de7a76ff2
Stew Esho <StewardEsho@gmail.com>
2016-04-28 10:36:19 -0400

    Enemies now derive from the baseEnemy class

--- en_aqua.lua
+++ en_aqua.lua
@@ -5,79 +5,26 @@
 -- en_aqua.lua
 --
 ------------------------------- Private Fields ---------------------------------
-local scene = require("scene");
-local physics = require("physics");
+local enemyBase = require("baseEnemy")
+local class = require("classy");
 
-aqua = {};
-aqua.__index = aqua;
------------------------------- Public Functions --------------------------------
+local M = {};
 
-function aqua.new(_x, _y, _index, _layer)
-  local instance = {
-  }
+M.class = class("Aquae", enemyBase.BaseEnemy);
+M.description = "Careful: Aquaes won't restrict themselves to one shape or size."
 
-  instance.x = _x or math.random(-1000, 1000);
-  instance.y = _y or math.random(-1000, 1000);
-  instance.index = index;
-  instance.layer = _layer or 1;
-  --------
-  instance.width = math.random(100, 500);
-  instance.height = math.random(100, 500);
-  instance.speed = 0;
-  instance.canShoot = true;
-  instance.maxSpeed = 25;
-  instance.acceleration = 0.45;
-  instance.isDead = false;
+function M.class:__init(_x, _y)
+  self.x = _x;
+  self.y = _y;
+  enemyBase.BaseEnemy.__init(self, 2, self.x, self.y, math.random(100, 500), math.random(100, 500), "img/sprites/aqua.png", "Aquae", description, 1);
 
-  instance.sprite = display.newRect(instance.x, instance.y, instance.width, instance.height);
-  instance.sprite.health = 70;
-  instance.sprite.armour = 15;
-  instance.sprite.name = "Aquae";
-  instance.sprite.enemyType = 2; --aquae
-  instance.sprite.description = "Careful: Aquaes won't restrict themselves to one shape or size.";
-  --Used for shaking the object when hit
-  instance.sprite.shakeMax = 15;
-  instance.sprite.shakeAmount = 0;
-  instance.sprite.isShaking = false;
+  self.speed = 0;
+  self.maxSpeed = 25;
+  self.acceleration = 0.25;
+  self.sprite.health = 65;
+  self.sprite.armour = math.random(12, 17);
 
-  return setmetatable(instance, aqua);
-end
-
-function aqua:shake()
-  if(self.sprite.isShaking == true) then
-    if(self.sprite.shakeMax <= 1) then
-      self.sprite.shakeMax = 15;
-      self.sprite.isShaking = false;
-    else
-      self.sprite.shakeAmount = math.random(self.sprite.shakeMax);
-      self.sprite.x = self.x + math.random(-self.sprite.shakeAmount, self.sprite.shakeAmount);
-      self.sprite.y = self.y + math.random(-self.sprite.shakeAmount, self.sprite.shakeAmount);
-      self.sprite.shakeMax = self.sprite.shakeMax - 0.85;
-    end
-  end
-end
-
-function aqua:kill()
-  self.sprite:removeSelf();
-end
-
-function aqua:init()
-  self.sprite.fill = {type = "image", filename = "img/sprites/aqua.png"};
   physics.addBody(self.sprite, "kinematic");
-  scene:addObjectToScene(self.sprite, self.layer);
 end
 
-function aqua:run()
-  if (self.sprite.health <= 0) then
-    self.isDead = true;
-  else
-    self:shake();
-    self.x = self.x + 0.25;
-    if(self.sprite.isShaking == false) then
-      self.sprite.x = self.x;
-      self.sprite.y = self.y;
-    end
-  end
-end
-
-return aqua;
+return M;
--- en_skeleton.lua
+++ en_skeleton.lua
@@ -5,79 +5,26 @@
 -- en_skeleton.lua
 --
 ------------------------------- Private Fields ---------------------------------
-local scene = require("scene");
+local enemyBase = require("baseEnemy")
+local class = require("classy");
 
-skeleton = {};
-skeleton.__index = skeleton;
------------------------------- Public Functions --------------------------------
+local M = {};
 
-function skeleton.new( _x, _y, index, _layer)
-  local instance = {
-  }
+M.class = class("Skeleship", enemyBase.BaseEnemy);
+M.description = "Fast and lightweight, Skeleships will weave through the brightest stars for their bounty."
 
-  instance.x = _x or math.random(-1000, 1000);
-  instance.y = _y or math.random(-1000, 1000);
-  instance.index = index;
-  instance.layer = _layer or 1;
+function M.class:__init(_x, _y)
+  self.x = _x;
+  self.y = _y;
+  enemyBase.BaseEnemy.__init(self, 1, self.x, self.y, 160, 200, "img/sprites/skel.jpg", "Skeleship", description, 1);
 
-  instance.width = 160;
-  instance.height = 200;
-  instance.sprite = display.newRect(instance.x, instance.y, instance.width, instance.height);
-  instance.speed = 0;
-  instance.canShoot = true;
-  instance.maxSpeed = 42;
-  instance.acceleration = 1;
-  instance.isDead = false;
+  self.speed = 0;
+  self.maxSpeed = 42;
+  self.acceleration = 1;
+  self.sprite.health = 30;
+  self.sprite.armour = math.random(10, 13);
 
-  --Used for shaking the object when hit
-  instance.sprite.shakeMax = 15;
-  instance.sprite.shakeAmount = 0;
-  instance.sprite.isShaking = false;
-
-  instance.sprite.health = 30;
-  instance.sprite.armour = math.random(10, 13);
-  instance.sprite.enemyType = 1; --skeleton
-  instance.sprite.name = "Skeleship";
-  instance.sprite.description = "Fast and lightweight, Skeleships will weave through the brightest stars for their bounty.";
-
-  return setmetatable(instance, skeleton);
-end
-
-function skeleton:shake()
-  if(self.sprite.isShaking == true) then
-    if(self.sprite.shakeMax <= 1) then
-      self.sprite.shakeMax = 15;
-      self.sprite.isShaking = false;
-    else
-      self.sprite.shakeAmount = math.random(self.sprite.shakeMax);
-      self.sprite.x = self.x + math.random(-self.sprite.shakeAmount, self.sprite.shakeAmount);
-      self.sprite.y = self.y + math.random(-self.sprite.shakeAmount, self.sprite.shakeAmount);
-      self.sprite.shakeMax = self.sprite.shakeMax - 0.85;
-    end
-  end
-end
-
-function skeleton:kill()
-  self.sprite:removeSelf();
-end
-
-function skeleton:init()
-  self.sprite.fill = {type = "image", filename = "img/sprites/skel.jpg"};
   physics.addBody(self.sprite, "kinematic");
-  scene:addObjectToScene(self.sprite, self.layer);
 end
 
-function skeleton:run()
-  if (self.sprite.health <= 0) then
-    self.isDead = true;
-  else
-    self:shake();
-    self.x = self.x + 0.25;
-    if(self.sprite.isShaking == false) then
-      self.sprite.x = self.x;
-      self.sprite.y = self.y;
-    end
-  end
-end
-
-return skeleton;
+return M;
