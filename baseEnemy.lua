16903816156396b32768673cc4c560c8cbd86113
Stew Esho <StewardEsho@gmail.com>
2016-05-02 10:36:24 -0400

    Added Fireball "collisions" with the player

--- baseEnemy.lua
+++ baseEnemy.lua
@@ -56,12 +56,14 @@ function M.BaseEnemy:shake()
   end
 end
 
+--Kills the enemy (does NOT remove from list of enemies)
 function M.BaseEnemy:kill()
   self.sprite.healthBar:removeSelf();
   self.sprite.healthMissing:removeSelf();
   self.sprite:removeSelf();
 end
 
+--Returns whether the enemy is dead or not
 function M.BaseEnemy:isDead()
   if(self.sprite ~= nil) then
     return false;
@@ -70,35 +72,58 @@ function M.BaseEnemy:isDead()
   end
 end
 
-function M.BaseEnemy:run()
-  if (self.sprite.healthBar.health <= 0) then
-    self.isDead = true;
-  else
-    self:shake();
-    if(self.sprite.isShaking == false) then
-      self.sprite.x = self.x;
-      self.sprite.y = self.y;
-    end
-    self.sprite.healthBar.width = (self.sprite.healthBar.health / self.sprite.healthBar.maxHealth) * self.sprite.healthMissing.width;
-
-    self.sprite.healthBar.y = self.sprite.y - (self.sprite.height/2) - 50;
-    self.sprite.healthBar.x = self.sprite.x - ((self.sprite.healthMissing.width - self.sprite.healthBar.width)/2);
-    self.sprite.healthMissing.y = self.sprite.y - (self.sprite.height/2) - 50;
-    self.sprite.healthMissing.x = self.sprite.x;
-  end
-end
-
+--Gets distance, in pixel widths, to a given point
 function M.BaseEnemy:getDistanceTo(_x, _y)
   local distance = math.sqrt(((self.x - _x) * (self.x - _x)) + ((self.y - _y) * (self.y - _y)));
   return distance;
 end
 
+--Given coordinates, returns angle from sprite to that point
 function M.BaseEnemy:getDirectionTo(_x, _y)
   local direction = math.deg(math.atan2((_y - self.y), (_x - self.x))) + 90;
   if (direction < 0) then
     direction = 360 + direction;
   end
   return direction;
+end
+
+function M.BaseEnemy:hasCollided( obj2 )
+  local obj1 = self.sprite;
+  if ( obj2 == nil ) then  -- Make sure the other object exists
+      return false
+  end
+
+  local left = obj1.contentBounds.xMin <= obj2.contentBounds.xMin and obj1.contentBounds.xMax >= obj2.contentBounds.xMin
+  local right = obj1.contentBounds.xMin >= obj2.contentBounds.xMin and obj1.contentBounds.xMin <= obj2.contentBounds.xMax
+  local up = obj1.contentBounds.yMin <= obj2.contentBounds.yMin and obj1.contentBounds.yMax >= obj2.contentBounds.yMin
+  local down = obj1.contentBounds.yMin >= obj2.contentBounds.yMin and obj1.contentBounds.yMin <= obj2.contentBounds.yMax
+
+  return (left or right) and (up or down)
+end
+
+function M.BaseEnemy:init()
+  --there seems to be nothing here.
+end
+
+function M.BaseEnemy:run()
+  --Checks if enemy is dead
+  if (self.sprite.healthBar.health <= 0) then
+    self.isDead = true;
+  else
+    --runs shake routine
+    self:shake();
+    if(self.sprite.isShaking == false) then
+      self.sprite.x = self.x;
+      self.sprite.y = self.y;
+    end
+
+    --sets healthbar size, and makes sure it follows the enemy's movement
+    self.sprite.healthBar.width = (self.sprite.healthBar.health / self.sprite.healthBar.maxHealth) * self.sprite.healthMissing.width;
+    self.sprite.healthBar.y = self.sprite.y - (self.sprite.height/2) - 50;
+    self.sprite.healthBar.x = self.sprite.x - ((self.sprite.healthMissing.width - self.sprite.healthBar.width)/2);
+    self.sprite.healthMissing.y = self.sprite.y - (self.sprite.height/2) - 50;
+    self.sprite.healthMissing.x = self.sprite.x;
+  end
 end
 
 return M;
